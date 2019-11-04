7879d9db70e5d84599956c91d350b3f261d032a0
DanJRM <49920289+DanJRM@users.noreply.github.com>
2019-11-04 08:26:55 -0600

    Update Hola.c

--- Hola.c
+++ Hola.c
@@ -1,11 +1,7 @@
-#include <stdio.h>
 #include <string.h>
-
 int main () {
-
    char saludo[6] = {'H', 'e', 'l', 'l', 'o', '\0'};
    strcpy ( saludo, "Adios" );
    printf("Saludo: %s\n", saludo );
    return 0;
 }
-
